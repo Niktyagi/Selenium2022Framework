395058982cd370ae24dfc1d5e5861797c45dba98
Nikhil Tyagi <nikhil.tyagi@instantsys.com>
2022-04-18 03:46:35 +0530

    feature 3

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -17,8 +17,11 @@ public class CartPage {
 		String f1 = "feature 1";
 		System.out.println(f1);
 		
-		String f2 = "feature 1";
+		String f2 = "feature 2";
 		System.out.println(f2);
+		
+		String f3 = "feature 3";
+		System.out.println(f3);
 	}
 
 }
