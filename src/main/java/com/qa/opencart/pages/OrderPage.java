ffb1bea0008b950f246cc0a93691ae609b4507dc
Niktyagi <niktyagi29@gmail.com>
2022-04-18 23:26:20 +0530

    Create OrderPage.java

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,17 @@
+package com.qa.opencart.pages;
+
+public class OrderPage {
+
+	public void makeOrder() {
+		System.out.println("order page......");
+	}
+	
+  public void getOrderId() {
+		System.out.println("order Id 1234......");
+	}
+  
+	public static void main(String[] args) {
+
+	}
+
+}
