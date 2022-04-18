a87e43344d3b5fb1843c359302baf08b6dd1ada8
Niktyagi <niktyagi29@gmail.com>
2022-04-18 23:11:42 +0530

    Create Sample.java

---
+++ src/main/java/com/qa/opencart/pages/Sample.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+public class Sample {
+
+	public void login() {
+		String username = "Raju";
+    String password = "Raj123"
+	}
+	
+	
+	public static void main(String[] args) {
+
+	}
+
+}
